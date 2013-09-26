774f0ea682414fcf4ca75f15b4ebec45fa69a09a
Singh <singh@ungrad9.astro.utoronto.ca>
2013-09-26 14:14:42 -0400

    updated

--- lab1/test1.py
+++ lab1/test1.py
@@ -1,8 +1,8 @@
 import numpy as np
 import PMT as pmt
 
-nsamp = 100
-tsamp = 0.001
+nsamp = 400
+tsamp = 0.08
 nrep = 6
 
 k = np.arange (nrep) +1
@@ -12,7 +12,7 @@ for i in k:
     x = pmt.photoncount(tsamp,nsamp)
     print x
     
-    myfilename = 'dark_{0}_{1}_{2}.dat'.format(i,tsamp,nsamp)
+    myfilename = 'LONGCOUNTtask7pmtrunno_{0}count{1}.dat'.format(i,tsamp)
     print 'Write data to: ' + myfilename
 
     np.savetxt(myfilename,x,fmt='%i')
